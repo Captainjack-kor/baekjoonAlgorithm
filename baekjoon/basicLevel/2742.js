0f8a776e783cb58a7c0c0bc44c23307a07b28cbc
Jack Lim <jennystr233@gmail.com>
2021-09-07 04:31:23 +0900

    add b2742

---
+++ baekjoon/basicLevel/2742.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let answer = "";
+
+rl.on('line', function (line) {
+  let Num = Number(line);
+
+  for(let i = Num; i >= 1; i--) {
+    answer += i + '\n';
+  }
+
+  rl.close();
+}).on('close', function () {
+  console.log(answer);
+  process.exit();
+});
