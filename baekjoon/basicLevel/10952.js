ed9e42ba32e2cbc43faa3ade3d6afbe24758292e
Jack Lim <jennystr233@gmail.com>
2021-09-07 08:38:12 +0900

    add b10952

---
+++ baekjoon/basicLevel/10952.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+  if(input.length -1 === Number(input[0])) {
+    rl.close();
+  }
+}).on('close', function () {
+  //input.shift();
+  //console.log(input);
+  for(let i = 0; i < input.length; i++) {
+    let result = input[i].split(' ');
+    //console.log(result);
+    let A = Number(result[0]);
+    let B = Number(result[1]);
+    //console.log(A + " " + B)
+    if(A + B !== 0) {
+      console.log(A + B);
+    }
+  }
+  process.exit();
+});
