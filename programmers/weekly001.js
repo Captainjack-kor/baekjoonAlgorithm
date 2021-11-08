26a3a7b999506dd95836ada0a7120c6ea1fd0c43
Jack Lim <jennystr233@gmail.com>
2021-11-08 23:27:19 +0900

    ready

---
+++ programmers/weekly001.js
@@ -0,0 +1,16 @@
+function solution(sizes) {
+  let answer = 0;
+  
+  let maxX = 0;
+  let maxY = 0;
+  let value = 0;
+
+  
+  
+  return answer;
+}
+
+
+console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]]));
+
+
