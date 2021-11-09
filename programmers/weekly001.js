5263343630e562ef8edd894a85fc30b582893ee8
Jack Lim <jennystr233@gmail.com>
2021-11-10 00:15:51 +0900

    algo ready

--- programmers/weekly001.js
+++ programmers/weekly001.js
@@ -3,10 +3,34 @@ function solution(sizes) {
   
   let maxX = 0;
   let maxY = 0;
-  let value = 0;
+  let newX = 0;
+  let newY = 0;
 
+  let result = 0;
+  let result2 = 0;
+
+  sizes.forEach(element => {
+    if(element[0] >= maxX) {
+      maxX = element[0];
+    } 
+    if(element[1] >= maxY) {
+      maxY = element[1];
+    }
+
+    result = maxX * maxY;
+
+    if(element[0] >= newY) {
+      newY = element[0];
+    }
+
+    if(element[1] >= newX) {
+      newX = element[1];
+    }
+
+    result2 = newX * newY;
+  });
   
-  
+  console.log(result, result2);
   return answer;
 }
 
