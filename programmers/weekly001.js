e6b6d7452a866735cbfb77f21282d5f00e8aa50e
Captainjack-kor <jennystr233@gmail.com>
2021-11-10 17:57:30 +0900

    이게 안된다고?

--- programmers/weekly001.js
+++ programmers/weekly001.js
@@ -3,38 +3,72 @@ function solution(sizes) {
   
   let maxX = 0;
   let maxY = 0;
-  let newX = 0;
-  let newY = 0;
 
   let result = 0;
   let result2 = 0;
-
-  sizes.forEach(element => {
-    if(element[0] >= maxX) {
-      maxX = element[0];
-    } 
-    if(element[1] >= maxY) {
-      maxY = element[1];
-    }
-
-    result = maxX * maxY;
-
-    if(element[0] >= newY) {
-      newY = element[0];
-    }
-
-    if(element[1] >= newX) {
-      newX = element[1];
-    }
-
-    result2 = newX * newY;
-  });
+    
+  let check = 0;
   
-  console.log(result, result2);
-  return answer;
-}
+  sizes.forEach((el) => {
+      let originX = 0;
+      let originY = 0;
+      let count = 0;
+
+      if(maxX < el[0] ) {
+          originX = maxX;
+          maxX = el[0];
+          count++;
+      } 
+      
+      if(maxY < el[1]) {
+          originY = maxY;
+          maxY = el[1];
+          count++;
+      }
+      
+      result = maxX * maxY;
+      console.log("maxX, maxY: " + maxX, maxY);
+      console.log("result1: " + result)
+
+      if(count >= 1) {
+          if(originX < el[1]) {
+              originX = el[1];
+          }
+          
+          if(originY < el[0]) {
+              originY = el[0];
+          }
+          
+          
+          console.log("originX, originY: " + originX, originY);
+          result2 = originX * originY;
+          console.log(result2);
+          
+      }
 
 
-console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]]));
+    if(result > result2) {
+      maxX = originX;
+      maxY = originY;
+    } 
+
+  })
+
+  if(result2 < result) {
+    return result2;
+  } else {
+    return result;
+  }
+} 
+  
+
+  
+  
+
+// console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]])); //기댓값: 4000
+// console.log(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]])); // 120
+console.log(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]])); // 133
+
+// console.log(solution([[60, 50], [30, 70]]));
 
 
