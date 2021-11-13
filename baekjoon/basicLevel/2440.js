15e24837578d6fe9537d1c3b814f5c54d680af24
Jack Lim <jennystr233@gmail.com>
2021-11-13 22:18:54 +0900

    2440 done

---
+++ baekjoon/basicLevel/2440.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = "";
+
+rl.on('line', function (line) {
+  // input.push(line);
+  input = Number(line);
+  // input = line;
+  rl.close();
+}).on('close', function () {
+  let str = "";
+  if(input === 0) {
+    console.log("");
+  }
+  // console.log(typeof(input));
+  else {
+    for(let i = 0; i < input; i++) {
+      for(let j = i; j < input; j++) {
+        str += "*";
+      }
+      // str += "\n";
+      console.log(str);
+      str = "";
+    }
+  }
+  // console.log(input);
+
+  process.exit();
+});
