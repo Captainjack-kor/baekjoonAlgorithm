326028edf7b5a6e5d242b6951a6752cef4670c46
Jack Lim <jennystr233@gmail.com>
2021-11-21 23:45:02 +0900

    DP

--- baekjoon/DP/1463.js
+++ baekjoon/DP/1463.js
@@ -12,18 +12,31 @@ rl.on('line', function (line) {
 }).on('close', function () {
   num = input;
   const DP = new Array(num + 1).fill(0);
-  console.log(DP);
+  //10개면 11개 만듬.
+
+  console.log(DP); 
+  //[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ...
+  
+  //1일땐 생각할 필요 없음. -> 문제가 1만들기 임
+
   for (let i = 2; i <= num; i++) { //2부터 입력 값까지
     DP[i] = DP[i - 1] + 1;
 
     if (i % 2 === 0) {
       DP[i] = Math.min(DP[i], DP[i / 2] + 1);
-      console.log(DP);
     }
 
     if (i % 3 === 0) {
       DP[i] = Math.min(DP[i], DP[i / 3] + 1);	
     }
+
+
+
+    console.log("-----------");
+    console.log("i: " + i);
+    console.log(DP);
+    console.log("-----------");
+
   }
 
   console.log(DP[num]);
