498e88d9a6120e80ffc6400d9cd7bc97067a861e
Jack Lim <jennystr233@gmail.com>
2021-12-05 23:33:48 +0900

    commit

--- programmers/level2/matchCutter.js
+++ programmers/level2/matchCutter.js
@@ -1,6 +1,20 @@
 function solution(s) {
     var answer = -1;
-    
+    let checkArr = [];
+    let sArr = s.split('');
+    console.log(sArr);
+    for(let i = 0; i < s.length; i++) {
+      if(s[i] === s[i + 1]) {
+        // test = s.slice(s.indexOf(s[i]), s.indexOf(s[i + 1]));
+      }
+      // if(checkArr.indexOf(s[i]) === -1) {
+      //   checkArr.push(s[i]);
+      // } else {
+      //   i++;
+      // }
+
+    }
+    // console.log(test);
 }
 
 
