28faaa4be86720cce93d6fc38a146cdc6ebcfedb
Jack Lim <jennystr233@gmail.com>
2021-12-06 00:09:17 +0900

    ready

--- Algorithm/practice/fatigueTest.js
+++ Algorithm/practice/fatigueTest.js
@@ -1,13 +1,25 @@
 function solution(k, dungeons) {
-  const {length} = dungeons
+  const { length } = dungeons;
+  // const length = dungeons.length;
+  // console.log(length);
   let isVisit = new Array(length).fill(false);
-  let result = []
+  console.log(isVisit);
+  let result = [];
   const aux = (visit, arr = []) => {
-      if(arr.length === length) return result.push(arr);
+    // console.log(visit);
+      if(arr.length === length) {
+        // for(let i = 0; i < arr.length; i++) {
+        //   if( k >= arr[])
+        // }
+        return result.push(arr);
+      }
+
       for(let i = 0; i < length; i++){
           if(!visit[i]){
               visit[i] = true;
-              aux(visit, arr.concat(i))
+              // console.log("🚀 visit", visit);
+              aux(visit, arr.concat(dungeons[i]));
+              //초깃 값이 [false, false, false] 다 넘어감.
               visit[i] = false;
           }
       }
@@ -15,3 +27,5 @@ function solution(k, dungeons) {
   aux(isVisit)
   console.log(result)
 }
+
+console.log(solution(80, [[80,20],[50,40],[30,10]]));
