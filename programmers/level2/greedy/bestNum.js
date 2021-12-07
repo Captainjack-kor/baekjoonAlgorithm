9b1faa3a4a28f32268f69fd5028e2db738dde0cb
Captainjack-kor <jennystr233@gmail.com>
2021-12-07 17:01:52 +0900

    use stack

--- programmers/level2/greedy/bestNum.js
+++ programmers/level2/greedy/bestNum.js
@@ -1,32 +1,46 @@
 function solution(number, k) {
   // Number.length - k = 총 선택의 갯수가 된다.
-  /*
-  왜 탐욕법이지... 정렬이안되는뎅 => 탐욕이 된당 ㅋ
-  일단 가장 큰 숫자를 먼저 찾기 "9" 그다음 -> "8" 9와 8사이의 
-  indexOf차이가 k 보다 작으면 왼쪽이냐 오른쪽이냐에 따라 문자열을 더해 줌.
-  그것이 최선일 가능이 높지 않을까? => 무한 반복 하면 이게 탐욕아닌가?
-  */
-
-  let strArr = number.split('');
+  
+  // 왜 탐욕법이지... 정렬이안되는뎅 => 탐욕이 된당 ㅋ
+  // 일단 가장 큰 숫자를 먼저 찾기 "9" 그다음 -> "8" 9와 8사이의 
+  // indexOf차이가 k 보다 작으면 왼쪽이냐 오른쪽이냐에 따라 문자열을 더해 줌.
+  // 그것이 최선일 가능이 높지 않을까? => 무한 반복 하면 이게 탐욕아닌가?
+  let greedyArr = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
+  let index = 0; // 가장 큰 값
+  
   var answer = '';
-  let choice = number.length - k;
-  let result = [];
-  const getPermutation = (arr = [], target = "") => {
-    // if(arr.length >= 1) {
-      for(let i = 0; i < arr.length; i++) {
-        const fixed = target + arr[i];
-        const copyArr = arr.slice();
-        copyArr.splice(i, choice);
-        if(fixed.length === choice) {
-          result.push(fixed);
-        }
-        getPermutation(copyArr, fixed)
-      }
-    // }
-  }
+  let leftIdx = 0;
+  // let rightIdx = greedyArr[index] + 1; // 보류 
+  let strArr = number.split('');
+  let allofcase = [];
+  let leftElement = 0;
+  let i = 0;
+  while(i < greedyArr.length) {
+    if(strArr.indexOf(greedyArr[index]) === -1) {
+        index++;
+    } else {
+      console.log("나는 " + index + "가 가장 높다 ");
+      //* 여기서 탐욕 적용!
+      // let rightIdx = greedyArr[index];
+      //if(k === 2)
+      // if(leftElement !== k) {
+      // strArr[rightIdx++]
+      // }
 
-  getPermutation(strArr);
-  console.log(result);
+      //범위가 
+      // strArr.length - k;
+      if(strArr.indexOf(greedyArr[index]) === -1) {
+          index++;
+      } else {
+        // k번쨰 만큼 떨어져있는가? 
+        // 
+      }
+    }
+    // (k - 1), 
+    i++;
+  }
+  
+  console.log(strArr);
   return answer;
 }
 
