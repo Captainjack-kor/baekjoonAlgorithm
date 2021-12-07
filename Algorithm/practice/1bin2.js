1c837994047878e0fafba5c0b3e75f7ec7894570
Captainjack-kor <jennystr233@gmail.com>
2021-12-07 09:26:18 +0900

    backup

---
+++ Algorithm/practice/1bin2.js
@@ -0,0 +1,19 @@
+//permutaion with visit
+
+function solution(k, dungeons) {
+    const {length} = dungeons
+    let isVisit = new Array(length).fill(false);
+    let result = []
+    const aux = (visit, arr = []) => {
+        if(arr.length === length) return result.push(arr);
+        for(let i = 0; i < length; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                aux(visit, arr.concat(i))
+                visit[i] = false;
+            }
+        }
+    }
+    aux(isVisit)
+    console.log(result)
+}
