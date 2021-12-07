faf5c2210f72c71ab4a8b1ec6b928e8aa4e6052c
Captainjack-kor <jennystr233@gmail.com>
2021-12-07 17:41:02 +0900

    save it

--- programmers/level2/greedy/bestNum.js
+++ programmers/level2/greedy/bestNum.js
@@ -1,46 +1,27 @@
 function solution(number, k) {
   // Number.length - k = 총 선택의 갯수가 된다.
   
-  // 왜 탐욕법이지... 정렬이안되는뎅 => 탐욕이 된당 ㅋ
   // 일단 가장 큰 숫자를 먼저 찾기 "9" 그다음 -> "8" 9와 8사이의 
   // indexOf차이가 k 보다 작으면 왼쪽이냐 오른쪽이냐에 따라 문자열을 더해 줌.
   // 그것이 최선일 가능이 높지 않을까? => 무한 반복 하면 이게 탐욕아닌가?
-  let greedyArr = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
+  // let greedyArr = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
   let index = 0; // 가장 큰 값
-  
+  let result = [];
   var answer = '';
-  let leftIdx = 0;
-  // let rightIdx = greedyArr[index] + 1; // 보류 
-  let strArr = number.split('');
-  let allofcase = [];
-  let leftElement = 0;
-  let i = 0;
-  while(i < greedyArr.length) {
-    if(strArr.indexOf(greedyArr[index]) === -1) {
-        index++;
+  for(let i = 0; i < number.length; i++) {
+    let prev = number[i];
+    let next = number[i + 1];
+    if(prev > next) {
+      result.push(prev);
+      k--;
     } else {
-      console.log("나는 " + index + "가 가장 높다 ");
-      //* 여기서 탐욕 적용!
-      // let rightIdx = greedyArr[index];
-      //if(k === 2)
-      // if(leftElement !== k) {
-      // strArr[rightIdx++]
-      // }
-
-      //범위가 
-      // strArr.length - k;
-      if(strArr.indexOf(greedyArr[index]) === -1) {
-          index++;
-      } else {
-        // k번쨰 만큼 떨어져있는가? 
-        // 
-      }
+      result.push(next);
+      k--;
     }
-    // (k - 1), 
-    i++;
+    // while() {
+
+    // }
   }
-  
-  console.log(strArr);
   return answer;
 }
 
