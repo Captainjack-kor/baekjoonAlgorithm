673c5b10a296c55fbc7922323a3544ee3bcdedbc
Captainjack-kor <jennystr233@gmail.com>
2021-12-07 09:18:05 +0900

    function isPrime

---
+++ Algorithm/practice/isPrime.js
@@ -0,0 +1,23 @@
+function isPrime(num) {
+  if(num < 2) {
+    return false;
+  }
+
+  if(num === 2) {
+    return true;
+  }
+
+  for(let i = 2; i < num / 2; i++) {
+    if(num % i === 0) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+console.log(isPrime(7)); //기댓값: true
+console.log(isPrime(8)); // false
+console.log(isPrime(10));  // false
+console.log(isPrime(5)); // false
+console.log(isPrime(2)); // false
