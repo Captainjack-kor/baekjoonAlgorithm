336b8e0895bd64f40df18d1d4feda6d06a572536
Captainjack-kor <jennystr233@gmail.com>
2021-12-07 09:18:23 +0900

    backup

--- programmers/level2/matchCutter2.js
+++ programmers/level2/matchCutter2.js
@@ -13,5 +13,20 @@ function solution(s) {
   return stack.length ? 0 : 1;
 }
 
+//! reference
+// function solution(s) {
+//   const stack = [];
+//     for(let i = 0 ; i < s.length ; i++ ){
+//       if(!stack.length || stack[stack.length - 1] !== s[i]) {
+//         stack.push(s[i]);
+//       } else {
+//         stack.pop();
+//       }
+//       console.log(stack);
+//   }
+
+//   return stack.length ? 0 : 1;
+// }
+
 console.log(solution("baabaa")); // 1
 console.log(solution("cdcd")); // 0
