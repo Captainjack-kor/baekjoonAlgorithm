4e5a9eddc64909bea5e3fdf99b18708fd084e97a
Captainjack-kor <jennystr233@gmail.com>
2021-12-07 13:35:08 +0900

    ready

--- programmers/level2/greedy/bestNum.js
+++ programmers/level2/greedy/bestNum.js
@@ -1,12 +1,31 @@
 function solution(number, k) {
- // number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 
- //가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
-  var answer = '';
-
+  // Number.length - k = 총 선택의 갯수가 된다.
   
+  // 왜 탐욕법이지... 정렬이안되는뎅 => 탐욕이 된당 ㅋ
+  // 일단 가장 큰 숫자를 먼저 찾기 "9" 그다음 -> "8" 9와 8사이의 indexOf차이가 k 보다 작으면 왼쪽이냐 오른쪽이냐에 따라 문자열을 더해 줌.
+  // 그것이 최선일 가능이 높지 않을까? => 무한 반복 하면 이게 탐욕아닌가?
+  let greedyArr = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
+  let index = 0; // 가장 큰 값
+  
+  var answer = '';
+  let strArr = number.split('');
+  
+  if(strArr.indexOf('9') === -1) {
+      console.log("없음")
+  }
+  
+  if(strArr.indexOf('7') === -1) {
+      console.log("7 없음")
+  }
+  if(strArr.indexOf('7') !== -1) {
+      console.log("7 있음")
+  }
+  
+  
+  
+  console.log(strArr);
   return answer;
 }
-
 
 /*
 number	k	return
