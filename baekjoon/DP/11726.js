026a7bd745b02ad183ab0c73a69de35123394188
Jack Lim <jennystr233@gmail.com>
2021-12-15 00:24:04 +0900

    ha

--- baekjoon/DP/11726.js
+++ baekjoon/DP/11726.js
@@ -4,13 +4,19 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let input = [];
+let input = "";
 
 rl.on('line', function (line) {
-  input.push(line);
+  // input.push(line);
+  input = line;
   rl.close();
 }).on('close', function () {
   console.log(input);
+
+  let getPermutations = (arr, selectNum) => {
+
+  };
+  getPermutations([], 3);
   process.exit();
 });
 
