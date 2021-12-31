95ec4b185ea178dc16f7874e06fa73e394328cd9
Captainjack-kor <jennystr233@gmail.com>
2021-12-31 10:54:41 +0900

    test

--- Algorithm/practice/DFS/DFStest01.js
+++ Algorithm/practice/DFS/DFStest01.js
@@ -1,3 +1,13 @@
+//* DFS Algorithm
+//! 출처: https://ryusm.tistory.com/48
+/*
+*자료구조 스택과 큐를 활용
+* needVisit 스택과 visited 큐, 두 개의 자료 구조를 생성한다.
+* 큐와 스택 구현은 별도의 라이브러리를 활용할 수도 있지만 간단히 배열을 통해 구현하자.
+* BFS 구조는 두 개의 큐를 활용하는데, DFS는 한 개의 스택과 한 개의 큐를 사용한다는 차이가 있음
+* BFS 구조는 이전 노드와 연결된 노드들을 먼저 탐색해야 하기 때문에 queue. 
+* DFS는 이전 노드가 아니라 자기 자신과 연결되었던 노드들 먼저 탐색하기 때문에 stack을 사용한다.
+*/
 const graph = {
   A: ["B", "C"],
   B: ["A", "D"],
@@ -22,7 +32,10 @@ const dfs = (graph, startNode) => {
   while (needVisitStack.length !== 0) {
     const node = needVisitStack.pop();
     if (!visitedQueue.includes(node)) {
+      //  없다면 진행
       visitedQueue.push(node);
+      // 노드 밀어넣고
+      // 재귀
       needVisitStack = [...needVisitStack, ...graph[node]];
     }
   }
--- programmers/level2/DFS/targetNumber.js
+++ programmers/level2/DFS/targetNumber.js
@@ -4,7 +4,6 @@ function solution(numbers, target) {
     let consoleCounter2 = 0;
     function dfs(index = 0, sum = 0) {
       consoleCounter++;
-      // console.log("그래 그리 쉽지는 않겠지 나를 허락해준 세상이란 " + consoleCounter);
       if(index === numbers.length) {
         if (sum === target) {
           answer++;
@@ -16,7 +15,6 @@ function solution(numbers, target) {
         dfs(index + 1, sum + numbers[index]);
       }
       consoleCounter2++;
-      // console.log("손쉽게 다가오는 편하고도 감미로운 공간이아냐 그래도 날아 오를꺼야 작은 날개 짓에 꿈을 담아 조금만 기다려봐 oh my love " + consoleCounter2);
       if(index < numbers.length) {
         dfs(index + 1, sum - numbers[index]);
       }
