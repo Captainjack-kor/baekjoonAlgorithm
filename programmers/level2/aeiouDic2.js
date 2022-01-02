4d4b3fe26de97f0a250e22a7d9b0b710b462d7d9
Jack Lim <jennystr233@gmail.com>
2022-01-03 01:02:45 +0900

    done

--- programmers/level2/aeiouDic2.js
+++ programmers/level2/aeiouDic2.js
@@ -4,16 +4,28 @@ function solution(word) {
   // 781
   // (n * 5) + 1
   // 6, 31, 156, 781, 
+
   let target_Value = [781, 156, 31, 6, 1];
+  // let target_Value = [1, 6, 31, 156, 781];
   
   let start = 0;
-  if(word.length === 1) {
-    for(let i = 0; i < vowels.length; i++) {
-      if(vowels[i] !== word[i]) {
-        answer += target_Value[target_Value.length - 1];
+  // if(word.length === 1) {
+    // for(let i = 0; i < vowels.length; i++) {
+    //   if(vowels[i] !== word[i]) {
+    //     answer += target_Value[target_Value.length - 1];
+    //   }
+    // }
+  // }
+  
+  for(let i = 0; i < 5; i++) {
+    for(let j = 0; j < vowels.length; j++) {
+      if(word[i] === vowels[j]) {
+        answer += (target_Value[i] * j) + 1;
       }
     }
   }
+  console.log(answer);
+
   // if(word[0] === "A") {
   //     answer += 1;
   // } else if (word[0] === "E") {
@@ -28,3 +40,11 @@ function solution(word) {
   
   return answer;
 }
+
+solution("AA");
+solution("AAA");
+solution("AAAE");
+solution("EIO");
+solution("E");
+solution("I");
+solution("O");
