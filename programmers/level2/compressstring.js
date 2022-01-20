9e103448f991654849c9ff961b7a35a83081b23d
Captainjack-kor <jennystr233@gmail.com>
2022-01-20 13:31:34 +0900

    70%

--- programmers/level2/compressstring.js
+++ programmers/level2/compressstring.js
@@ -10,49 +10,55 @@ function solution(s) {
   //8개 "2ababcdcd" => length: 9
 
   // 무조건 스택아님?
-  let count = 0;
-  // let start = 1;
-  let max = 0;
+  let min = s.length;
   let stack = [];
-  // let result = [];
+  let result = [];
 
-  // for(let i = 0; i < s.length; i++) {
+  for(let i = 1; i <= s.length; i++) {
     // 일단 다빠지면 정지하는거 넣기 s.length !== 0
+
+    let count = 0;
     let temp = "";
     //* "ababcdcdababcdcd"
-    //* "aabbaccc" => 2a | 2b | a3c = 7
+    //* "aabbacccc" => 2a | 2b | a | 4c = 7
+    // for(let j = 0; j < Math.floor(s.length / i); j++) {
     for(let j = 0; j < s.length; j++) {
       // temp = s.substring(j, (j + i) * 2);
-      temp = s.substring(j + 1, j); 
+      // i가 하나 커질떄마다 s.substring(i, 0)
+      temp = s.substring((j + 1) * i, j * i); 
+      
       // console.log(temp);
-      if(stack[stack.length - 1] !== temp && temp !== undefined) {
+      if(stack[stack.length - 1] !== temp && temp !== undefined && temp !== '') {
         stack.push(temp);
+        result.push(count);
+        count = 0;
       } else if(stack[stack.length - 1] === temp){
+      // } else {
+        // console.log(stack[stack.length - 1]); //5 => a, b, c, c, c
+        // count -= temp.length;
+        console.log(temp);
         count++;
-      }
+        console.log(count);
+      } 
       temp = "";
     }
+    console.log("Stack: " + stack);
+    console.log("Count: " + count)
 
-    // if(max < count) {
-      // max = count;
-    // } 
-    console.log(count);
-    if(stack.length + count > max)  {
-      max = stack.length + count;
-    }
-    
     count = 0;
     stack = [];
 
-    console.log(max);
+    console.log(result);
 
-    return max;
-  // }
+    // return min;
+  }
 }
 
 
-// solution("ababcdcdababcdcd");
+// solution("ababcdcdababcdcd"); // 9
+// solution("ababcdcdababc         dcd"); // 9
 solution("aabbacccc"); // 7
+// solution("abcabcabcabcdededededede"); // 14
 //2a2ba4c => 7; 
 // solution("abac"); // 7
 // solution("2a2ba3c"); // 7
