a1b78d21d0fac20e47bed4f249967af87cdd81a4
Captainjack-kor <jennystr233@gmail.com>
2022-01-21 17:26:27 +0900

    egae oj

--- programmers/level2/delivery.js
+++ programmers/level2/delivery.js
@@ -1,3 +1,5 @@
+// const { cloneEnvironment } = require("estraverse");
+
 function solution(N, road, K) {
   var answer = 0;
 
@@ -9,36 +11,28 @@ function solution(N, road, K) {
 		return a;
 	}, 0);
 
-  // 이 레퍼런스는 인접 리스트로 만듭니다. (행렬도 가능합니다. 행렬로 작성해 보세요.)
 	const adjList = {};
 
-  // 인접 리스트에 최대 버텍스 크기만큼 반복해 버텍스를 만들어 줍니다.
 	for (let i = 0; i <= maxVertex; i++) {
 		adjList[i] = [];
 	}
 
 	for (let i = 0; i < edges.length; i++) {
-		adjList[edges[i][0]].push(edges[i][1]);
-		adjList[edges[i][1]].push(edges[i][0]);
+		adjList[edges[i][0]].push({ "vertex" : edges[i][1], "distance" : road[i][2]});
+		adjList[edges[i][1]].push({ "vertex" : edges[i][0], "distance" : road[i][2]});
+		// adjList[edges[i][0]].push(edges[i][1]);
+		// adjList[edges[i][1]].push(edges[i][0]);
 	}
 
   console.log(adjList);
-  // 방문한 버텍스를 담을 객체를 선언합니다.
+
 	const visited = {};
-	// 컴포넌트가 몇 개인지 카운트할 변수를 선언합니다.
+
 	let count = 0;
 
-  // 그래프에 있는 버텍스를 전부 순회합니다.
 	for (let vertex = 0; vertex <= maxVertex; vertex++) {
-
-		// 만약 i 번째 버텍스를 방문하지 않았다면 bfs로 해당 버텍스와, 버텍스와 연결된(간선) 모든 버텍스를 방문합니다.
-		// BFS로 갈 수 있는 모든 정점들을 방문하며 visited에 담기 때문에, 방문한 버텍스는 visited에 들어 있어서 bfs를 돌지 않습니다.
-		// 이렇게 컴포넌트를 확인합니다.
 		if (!visited[vertex]) {
-			// 그래프와 버텍스, 방문했는지 확인할 visited를 변수에 담습니다.
-			bfs(adjList, vertex, visited);
-
-			// 카운트를 셉니다.
+			bfs(adjList, vertex, visited, K);
 			count++;
 		}
 	}
@@ -47,39 +41,49 @@ function solution(N, road, K) {
 	return count;
 } 
 
-function bfs(adjList, vertex, visited) {
-
-	// bfs는 가장 가까운 정점부터 탐색하기 때문에 queue를 사용합니다.
-	// queue에 vertex를 담습니다.
+function bfs(adjList, vertex, visited, K) {
 	const queue = [vertex];
-	// 해당 버텍스를 방문했기 때문에 visited에 담아 주고, 방문했다는 표시인 true를 할당합니다.
+
 	visited[vertex] = true;
 
-  
-  // queue의 길이가 0일 때까지 순환합니다.
 	while (queue.length > 0) {
+    let sum = 0;
+		var cur = queue.shift();
+    // console.log(cur);
+    // console.log(adjList[cur].length);
+    // console.log(adjList[cur].length);
+    // if(adjList[cur].length !== undefined) {
+      for (let i = 0; i < adjList[cur].length; i++) {
+        // console.log(adjList[cur][i][]);
 
-		// cur 변수에 정점을 할당합니다.
-		// queue는 선입선출이기 때문에 shift 메소드를 사용하여 버텍스를 가져옵니다.
-		const cur = queue.shift();
+        // console.log(road[i][0]);
+        // console.log(adjList[cur][i]);
+        // console.log("length: " + Number(road[i][2]));
+        // sum += road[i][2];
 
+        if (!visited[adjList[cur][i].vertex]) {
+          // console.log(adjList[cur][i].vertex);
 
-		// 그래프의 cur 정점에 있는 간선들을 전부 순회합니다.
-		for (let i = 0; i < adjList[cur].length; i++) {
+          sum += adjList[cur][i].distance;
+          console.log(adjList[cur][i].distance);
+          queue.push(adjList[cur][i].vertex);
+          console.log(queue);
+          visited[adjList[cur][i].vertex] = true;
+          // console.log(visited);
+          // console.log("K: " + K);
+        }
+        console.log("sum: " + sum);
+        console.log("오늘은 여기까지");
+    
+      }
+    sum = 0;
+    // }
 
-			// 만약, 해당 버텍스를 방문하지 않았다면 queue에 삽입합니다.
-			// 방문했다는 표시로 visited에 해당 버텍스를 삽입하고 true를 할당합니다.
+    // console.log(adjList[cur])
+  }
 
-			if (!visited[adjList[cur][i]]) {
-				queue.push(adjList[cur][i]);
-				visited[adjList[cur][i]] = true;
-			}
-
-			// queue에 다음으로 방문할 버텍스가 있기 때문에 while은 멈추지 않습니다.
-			// 만약, queue가 비어 있다면 더 이상 갈 곳이 없는 것이기 때문에 bfs 함수를 종료하고 카운트를 셉니다.
-      
-		}
-	}
+    // console.log(sum);
+    // console.log("오늘은 여기까지");
 }
 
 
