f3ef9d29219110d7afcf420b0d6d3bf2c70f787e
Captainjack-kor <jennystr233@gmail.com>
2022-01-24 16:03:11 +0900

    다익스트라

---
+++ programmers/level2/deliverySolution3.js
@@ -0,0 +1,22 @@
+// https://m.blog.naver.com/ndb796/221234424646
+
+function getSmallIndex() {
+  let min = INF;
+  let index = 0;
+  for(let i = 0; i < N; i++) {
+
+  }
+
+  return index;
+}
+
+function solution(N, road, K) {
+  
+  for(let i = 0; i < N - 2; i++) {
+    let current = getSmallIndex();
+    
+  }
+
+}
+
+solution("5", [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]], 3);
