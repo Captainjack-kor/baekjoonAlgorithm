5a79aef1c03c75e24315fe6b59844c3fe3fb249c
Captainjack-kor <jennystr233@gmail.com>
2022-01-24 17:58:16 +0900

    50%

--- programmers/level2/deliverySolution3.js
+++ programmers/level2/deliverySolution3.js
@@ -3,15 +3,38 @@
 function getSmallIndex() {
   let min = INF;
   let index = 0;
-  for(let i = 0; i < N; i++) {
-
-  }
-
-  return index;
+  // for(let i = 0; i < N; i++) {
+  // }
+  // return index;
 }
 
 function solution(N, road, K) {
-  
+  let arr = [];
+  // let INF = "10000";
+
+  for(let i = 0; i <= N; i++) {
+    // arr[i].push(i);
+    arr.push(Array(Number(N)).fill("INF"));
+    // if(arr[i][0])
+  }
+
+  for(let i = 0; i < road.length; i++) {
+    arr[i][i] = 0;
+    arr[road[i][0]][road[i][1]] = road[i][2];
+    arr[road[i][1]][road[i][0]] = road[i][2];
+    
+    // arr[road[i][0] - 1][road[i][1] - 1] = road[i][2] - 1;
+    // arr[road[i][1] - 1][road[i][0] - 1] = road[i][2] - 1;
+
+    // console.log(arr[road[i][0]][road[0][1]]);
+  }
+  // for (let i = 0; i < road.length; i++) {
+  //   if(obj[i] =
+	// }
+  arr.shift();
+  console.log(arr);
+
+
   for(let i = 0; i < N - 2; i++) {
     let current = getSmallIndex();
     
