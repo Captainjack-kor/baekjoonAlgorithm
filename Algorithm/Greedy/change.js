08155e25b1d67d912ad1a3abd8f13a78ba37efec
Captainjack-kor <jennystr233@gmail.com>
2022-01-26 11:21:23 +0900

    change done

--- Algorithm/Greedy/change.js
+++ Algorithm/Greedy/change.js
@@ -5,16 +5,22 @@
 //* 큰것들은 작은 수의 배수니까 정렬 해서 접근
 
 
-function solution() {
-  let n = 1260;
+function solution(num) {
+  let n = num;
   let count = 0;
 
   let arr = [500, 100, 50, 10];
 
-  for(let i = 0; i < arr.length; i++) {
-    console.log(i);
+  for(let coin of arr) {
+    count += Math.floor(n / coin); // 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+    // 0 = 0 + 1260;
+    n %= coin;
+    // n = n % coin; 1260 / 500 
+    // count++;
+    console.log("coin: " + n);
   }
+  console.log(count);
 }
 
 
-solution()
+solution(1260);
