ded0ad3433dc4f9579a08c84aba21e8697d1d109
Captainjack-kor <jennystr233@gmail.com>
2022-01-27 11:01:09 +0900

    done

--- programmers/level2/atruckcrossingthebridge.js
+++ programmers/level2/atruckcrossingthebridge.js
@@ -1,45 +1,43 @@
 function solution(bridge_length, weight, truck_weights) {
-  var answer = 0;
   let count = 0;
-  // let crossing_truck_list = Array(bridge_length).fill(0);
-  let crossing_truck_list = [];
-  // console.log(crossing_truck_list);
-  let after_crossing_truck_list = [];
-  let end_point = truck_weights.length;
-  console.log(end_point);
-  while(end_point !== after_crossing_truck_list.length) { 
-    
-    let weight_sum = crossing_truck_list.reduce((acc,cur) => acc + cur, 0);
-    
-    if(weight_sum + truck_weights[0] <= weight && truck_weights[0] !== undefined) {
-      // crossing_truck_list.shift();
-      crossing_truck_list.push(truck_weights.shift());
-      // count += bridge_length;
-    } 
-    
-    else {
-      count += bridge_length;
-      count += crossing_truck_list.length;
+  let crossing_truck_list = Array(bridge_length - 1).fill(0);
+  // let crossing_truck_list = [];
 
-      while(crossing_truck_list.length > 0) {
-        after_crossing_truck_list.push(crossing_truck_list.shift());
-      }
-      
-      if(truck_weights.length > 0) {
-        crossing_truck_list.push(truck_weights.shift());
-      }
-      
-      
-      console.log("count: " + count);
+  crossing_truck_list.push(truck_weights.shift());
+  // console.log(crossing_truck_list);
+
+  while(crossing_truck_list.length > 0) {
+
+    // count += bridge_length;
+    count++;
+    // crossing_truck_list.pop();
+    let weight_sum = crossing_truck_list.reduce((acc,cur) => acc + cur, 0);
+    if(weight_sum === 0 && truck_weights.length === 0) {
+      break;
     }
+ 
+    if(weight_sum + truck_weights[0] <= weight && truck_weights[0] !== undefined) {
+      crossing_truck_list.push(truck_weights.shift()); 
+      crossing_truck_list.shift();
+    } else {
+      crossing_truck_list.shift();
+      let weight_sum = crossing_truck_list.reduce((acc,cur) => acc + cur, 0);
+      if(weight_sum + truck_weights[0] <= weight && truck_weights[0] !== undefined) {
+        crossing_truck_list.push(truck_weights.shift());
+      } else {
+        crossing_truck_list.push(0);
+      }
+    }
+
     console.log(crossing_truck_list);
   }
 
+
   console.log(count);
-  return answer;
+  return count;
 }
 
-solution(2, 10, [7,4,5,6]); // 8
+// solution(2, 10, [7,4,5,6]); // 8
 // solution(100, 100, [10]); // 101
 // solution(100, 100, [10,10,10,10,10,10,10,10,10,10]); // 110
 
