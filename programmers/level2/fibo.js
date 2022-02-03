d21ae61c9a059482dda211cda56b5f66b6876e61
Captainjack-kor <jennystr233@gmail.com>
2022-02-03 19:45:46 +0900

    done

---
+++ programmers/level2/fibo.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  let answer = fib_bottom_up(n) % 1234567;
+  return answer;
+}
+
+function fib_bottom_up(v) {
+  let bottom_up = [];
+  bottom_up[0] = 0;
+  bottom_up[1] = 1;
+  bottom_up[2] = 1;
+  for (let i = 3; i <= v; i++) {
+      bottom_up[i] = (bottom_up[i - 1] % 1234567 + bottom_up[i - 2] % 1234567);
+  }
+  return bottom_up[v];
+}
+solution(3);
+solution(5);
