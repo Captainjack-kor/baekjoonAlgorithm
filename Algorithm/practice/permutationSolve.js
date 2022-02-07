d0e14d66e5eb29aaddd85b7d4a48433ac8c07ce7
Captainjack-kor <jennystr233@gmail.com>
2022-02-07 11:01:09 +0900

    temp save

---
+++ Algorithm/practice/permutationSolve.js
@@ -0,0 +1,45 @@
+function solution(k, dungeons) {
+  var answer = [];
+
+// 순서만드는 재귀함수
+//     let test = [1,2,3,4,5];
+//     let result = [];
+
+//     let make = function(rt, target) {
+//         if( target.length == 0 ){
+//             result.push(rt);
+//         }
+
+//         for(let i=0; i<target.length; i++){
+//             let temp = target.slice(0,i).concat(target.slice(i+1));
+//             let copy = rt.slice();
+//             copy.push(target[i]);
+//             make(copy, temp);
+//         }
+//     }
+
+//     make([],test);
+
+
+  let make = function(eg, target) {
+      if( target.length == 0 ){
+          answer.push(dungeons.length);
+      }
+
+      for(let i=0; i<target.length; i++){
+          let now = target[i];
+          if( eg < now[0] ){
+              answer.push(dungeons.length-target.length);
+          }else{
+              let temp = target.slice(0,i).concat(target.slice(i+1));
+              make(eg - now[1], temp);
+          }
+      }
+  }
+
+  make(k,dungeons);
+
+  return Math.max.apply(null, answer);
+}
+
+solution(3, [1, 2, 3]);
--- programmers/level2/joystick.js
+++ programmers/level2/joystick.js
@@ -22,6 +22,7 @@
 function permutation(k, arr) {
   // K가 총 A의 갯수
   const { length } = arr
+  // console.log("arr: " + length);
   let isVisit = new Array(length).fill(false);
   let result = [];
   const aux = (visit, arr = []) => {
@@ -40,7 +41,8 @@ function permutation(k, arr) {
       for(let i = 0; i < length; i++){
           if(!visit[i]){
               visit[i] = true;
-              aux(visit, arr.concat(i))
+              aux(visit, arr.concat(i));
+              // aux(visit, arr.concat(i))
               visit[i] = false;
           }
       }
@@ -190,7 +192,7 @@ function solution(name) {
 }
 
 solution("JAZ"); // 11
-solution("JEROEN"); // 56 
+// solution("JEROEN"); // 56 
 // solution("AA");
 // solution("AAA");
 // solution("JAN"); // 23
