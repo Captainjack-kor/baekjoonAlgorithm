abe7c3d261edadf681fa5ed4d357b70671ca611e
Captainjack-kor <jennystr233@gmail.com>
2022-02-09 09:00:00 +0900

    dfs

--- Algorithm/practice/1bin2.js
+++ Algorithm/practice/1bin2.js
@@ -17,3 +17,28 @@ function solution(k, dungeons) {
     aux(isVisit)
     console.log(result)
 }
+
+
+// function solution(k, dungeons) {
+//     const { length } = dungeons;
+//     let isVisit = new Array(length).fill(false);
+//     let result = 0;
+//     const aux = (visited, rest = k, depth = 0) => {
+//         if(!visited.includes(false) || depth === length){
+//             if(result < depth) result = depth;
+//             return;
+//         }
+//         for(let i = 0; i < length; i++){
+//             if(!visited[i]){
+//                 visited[i] = true;
+//                 if(dungeons[i][0] <= rest){
+//                     aux(visited, rest - dungeons[i][1], depth + 1);
+//                 }
+//                 visited[i] = false;
+//             }
+//         }
+//         return result < depth ? result = depth : result
+//     }
+//     aux(isVisit)
+//     return result;
+// }
