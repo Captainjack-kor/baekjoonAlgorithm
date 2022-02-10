08bab71e6481139a4cd8706f955f9da5bde32512
Captainjack-kor <jennystr233@gmail.com>
2022-02-10 16:59:29 +0900

    ㅋㅋ

--- Algorithm/practice/1bin2.js
+++ Algorithm/practice/1bin2.js
@@ -5,7 +5,10 @@ function solution(k, dungeons) {
     let isVisit = new Array(length).fill(false);
     let result = []
     const aux = (visit, arr = []) => {
-        if(arr.length === length) return result.push(arr);
+        if(arr.length === length) {
+            return result.push(arr);
+        }
+        
         for(let i = 0; i < length; i++){
             if(!visit[i]){
                 visit[i] = true;
