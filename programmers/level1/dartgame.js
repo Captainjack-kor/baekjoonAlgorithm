11d04a812986e767e25faec549a2d108e4bd6a52
Captainjack-kor <jennystr233@gmail.com>
2022-02-20 23:59:47 +0900

    1

--- programmers/level1/dartgame.js
+++ programmers/level1/dartgame.js
@@ -1,4 +1,31 @@
 function solution(dartResult) {
   var answer = 0;
+
+  let point = 0;
+  for(let i = 0; i < dartResult.length; i++) {
+    // console.log(typeof(dartResult[i]))
+    if((Number(dartResult[i])) <= 10) {
+      point = Number(dartResult[i]);
+    } else if(dartResult[i] === 'S') {
+      point = Math.pow(point, 1);
+    } else if(dartResult[i] === 'D') {
+      point = Math.pow(point, 2);
+    } else if(dartResult[i] === 'D') {
+      point = Math.pow(point, 3);
+    } else if(dartResult[i] === '*') {
+      point *= 2;
+    } else if(dartResult[i] === '#') {
+      point = -point;
+    }
+    answer = point;
+    point = 0;
+  }
+
+  console.log(answer)
   return answer;
 }
+
+// console.log(Math.pow(3,2));
+
+solution("1S2D*3T"); // 37 [11 * 2 + 22 * 2 + 33]
+solution("1D2S#10S"); // 9 [12 + 21 * (-1) + 101]
