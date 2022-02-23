1c38750802380e2a1d5d32f66a625a11033c4aa1
Captainjack-kor <jennystr233@gmail.com>
2022-02-23 17:55:31 +0900

    pause

---
+++ baekjoon/Bronze/Bronze2/14659.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+  if(input.length === 2) {
+    rl.close();
+  }
+}).on('close', function () {
+  console.log(input);
+  let arr = [];
+  arr = input[1].split(' ');
+  let count = 0;
+  let maxCount = 0;
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = i; j < arr.length; j++) {
+      if(Number(arr[i]) <= Number(arr[j])) {
+        count++;
+      } else {
+        break;
+      }
+    }
+
+    if(count > maxCount) {
+      maxCount = count;
+    } 
+    
+    count = 0;
+  }
+  console.log(maxCount);
+  // console.log(arr);
+  process.exit();
+});
+
