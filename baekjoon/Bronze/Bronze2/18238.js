3da5556631942e64b1492ddb34c2e619fb7deed6
Captainjack-kor <jennystr233@gmail.com>
2022-03-03 12:36:10 +0900

    ZOAC 컷

--- baekjoon/Bronze/Bronze2/18238.js
+++ baekjoon/Bronze/Bronze2/18238.js
@@ -14,55 +14,37 @@ rl.on('line', function (line) {
   let strArr = ["A","B","C","D","E","F","G", "H", 
   "I", "J", "K", "L", "M", "N", "O", "P", "Q", 
   "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  let reverse_strArr = strArr.reverse();
-  // console.log(reverse_strArr);
-  // 왼쪽, 오른쪽 두 경우만 고려 (조이스틱의 순한 맛)
-  let result_count = 0;
+  // let reverse_strArr = strArr.reverse();
+
+  let move = 0;
   let target = "A";
-  for(let i = 0; i < input.length; i++) {
-    let temp_target_L = "";
-    let temp_target_R = "";
-    let left_count = 0;
-    let right_count = 0;
-    // 출발 if(input[i].indexOf)
-    // 상시
+  let left_distance = 0;
+  let right_distance = 0;
 
-    // console.log(input[i - 1]);
+  let init = 0;
+  
+  let init_temp_L = Math.abs('A'.charCodeAt() - input[0].charCodeAt());
+  let init_temp_R = 26 - Math.abs('A'.charCodeAt() - input[0].charCodeAt());
 
-    // if(input[i - 1] === undefined) {
-    //   input[i - 1] = "A";
-    // }
-    // console.log(input[i - 1]);
-    console.log("TARGET: " + target);
-    for(let j = strArr.indexOf(target); j < strArr.length; j++) {
-      right_count++;
-      console.log("오른쪽으로 : " + strArr[j]);
-      if(strArr[j] === input[i]) {
-        temp_target_R = strArr[j];
-        console.log(temp_target_R + "RRR");
-        console.log("str: " + strArr[j] +' '+right_count);
-        break;
-      }
-    }
+  if(init_temp_L < init_temp_R) {
+    init = init_temp_L;
+  } else {
+    init = init_temp_R;
+  }
 
-    for(let z = reverse_strArr.indexOf(target); z < reverse_strArr.length; z++) {
-      left_count++;
-      console.log("왼쪽으로 : " + reverse_strArr[z]);
-      if(reverse_strArr[z] === input[i]) {
-        temp_target_L = reverse_strArr[z];
-        console.log(temp_target_L + "LLL");
-        console.log("reverse: " + reverse_strArr[z] +' '+ left_count);
-        break;
-      }
-    }
-    if(left_count > right_count) {
-      result_count += right_count;
-      target = temp_target_R;
+  for(let i = 0; i < input.length - 1; i++) {
+    left_distance = Math.abs(input[i + 1].charCodeAt() - input[i].charCodeAt());
+    right_distance = 26 -  Math.abs(input[i + 1].charCodeAt() - input[i].charCodeAt())
+
+    if(left_distance < right_distance) {
+      move += left_distance;
     } else {
-      result_count += left_count;
-      target = temp_target_L;
+      move += right_distance;
     }
   }
-  console.log(result_count);
+  
+  move += init;
+  
+  console.log(move);
   process.exit();
 });
