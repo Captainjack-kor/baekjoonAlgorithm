e847ee7521928dcfdbcc1f413b193982a3a41afa
Captainjack-kor <jennystr233@gmail.com>
2022-03-07 16:58:22 +0900

    내풀이

---
+++ baekjoon/Silver/Silver1/10844.js
@@ -0,0 +1,9 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let answer = Number(input[0]);
+
+// 1 2 3 4 5 6 7 8 9 -> 9
+// 13 21 23 32 34 43 45 54 56 65 67 76 78 87 89 98 100 -> 17
+
+// 121 123 232 234 343 345
+
--- baekjoon/Silver/Silver3/9095.js
+++ baekjoon/Silver/Silver3/9095.js
@@ -9,6 +9,9 @@ memo[1] = 1;
 memo[2] = 2;
 memo[3] = 4;
 
+//결국 어떤 앞의 수에서 끝에 1, 2, 3을 더하는 것이 되므로, 
+//현재값 -1, 현재값 -2, 현재값 -3의 횟수를 더한 값이 현재값의 총 횟수인 것이다.
+
 for(let i = 1; i <= input.length - 1; i++) {
   let num = input[i];
   for(let j = 4; j <= num; j++) {
