11a0111eac7e238dc42f8a45345cbc6e9d39175e
Captainjack-kor <jennystr233@gmail.com>
2022-03-18 17:58:28 +0900

    1018 집에서...

---
+++ baekjoon/Silver/Silver5/1018.js
@@ -0,0 +1,62 @@
+const [input, ...chess] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let [N, M] = input.split(' ');
+// console.log(N, M);
+// console.log(Number(N) === 8);
+// console.log(chess);
+
+let arr_type1 = [
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+]
+
+let arr_type2 = [
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+]
+
+function compare_type1(y, x) {
+  let count = 0;
+  for(let i = y; i < y + 8; i++) {
+    for(let j = x; j < x; j++) {
+      if(input[i][j] !== arr_type1[i - y][j - x]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function compare_type2(y, x) {
+  let count = 0;
+  for(let i = y; i < y + 8; i++) {
+    for(let j = x; j < x; j++) {
+      if(input[i][j] !== arr_type2[i - y][j - x]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+let result = [];
+for(let i = 0; i + 7 < Number(N); i++) {
+  for(let j = 0; j + 7 < Number(M); j++) {
+    result.push(compare_type1(j, i));
+    result.push(compare_type2(j, i));
+  }
+}
+
+console.log(result);
