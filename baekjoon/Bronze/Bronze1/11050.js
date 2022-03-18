f7432beee98e486b2093cc79f0ebb35720b7e552
Captainjack-kor <jennystr233@gmail.com>
2022-03-18 09:25:36 +0900

    backup

--- baekjoon/Bronze/Bronze1/11050.js
+++ baekjoon/Bronze/Bronze1/11050.js
@@ -3,7 +3,7 @@ const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("
 let N = +input[0].split(' ')[0];
 let K = +input[0].split(' ')[1];
 
-console.log(N, K);
+// console.log(N, K);
 let target_N = 1;
 let target_K = 1;
 let target_NK = 1; 
@@ -19,7 +19,7 @@ for(let i = 1; i <= N - K; i++) {
   target_NK *= i;
 }
 
-console.log(target_N, target_K, target_NK);
+// console.log(target_N, target_K, target_NK);
 let answer = 1;
 
 answer = target_N / (target_K * target_NK);
