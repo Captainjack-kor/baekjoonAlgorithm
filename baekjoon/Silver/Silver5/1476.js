7ea5c0daa264301bec90226fe889d236312021d5
Captainjack-kor <jennystr233@gmail.com>
2022-03-23 17:53:11 +0900

    git merged

--- baekjoon/Silver/Silver5/1476.js
+++ baekjoon/Silver/Silver5/1476.js
@@ -9,13 +9,16 @@ rl.on("line", (line) => {
   let [E, S, M]  = line.split(' ').map(v => +v);
 
     let count = 1;
-    while(true) {
-      if((count - E) % 15 === 0 && (count - S) % 28 === 0 && (count - M) % 19 === 0) {
-        console.log(count);
-        process.exit();
-      } else { 
+    while (true) {
+        if (
+            (count - E) % 15 === 0 &&
+            (count - S) % 28 === 0 &&
+            (count - M) % 19 === 0
+        ) {
+            console.log(count);
+            process.exit();
+        }
         count++;
-      }
     }
 });
 
