788bcc2b88652a10f47c8018f00892e17c8821f4
Jack Lim <jennystr233@gmail.com>
2022-04-03 23:08:47 +0900

    festival 1

--- baekjoon/festival/1.js
+++ baekjoon/festival/1.js
@@ -1,2 +1,5 @@
 console.log("                                                           :8DDDDDDDDDDDDDD$.                                           ");
-console.log("                                                      DDDNNN8~~~~~~~~~~=~7DNNDNDDDNNI                                   ")
+console.log("                                                      DDDNNN8~~~~~~~~~~=~7DNNDNDDDNNI                                   ");
+console.log("                                                  ?NNDD=~=~~~~~~~~~~~~~~~~~=~~==~=INNDNN7                                ");
+console.log("                                               +NDDI~~~~~~~~~~~~~~~~~~~~~~~=~~========~ODND+                            ")
+console.log("                                           :NND~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~============7NDN                          ")
