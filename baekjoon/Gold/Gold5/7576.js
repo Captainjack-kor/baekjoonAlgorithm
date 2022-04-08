2e55d47c82d1bbe826f09c16e62687e04029b94d
Captainjack-kor <jennystr233@gmail.com>
2022-04-08 10:26:59 +0900

    토마토 컷;;

--- baekjoon/Gold/Gold5/7576.js
+++ baekjoon/Gold/Gold5/7576.js
@@ -1,83 +1,49 @@
 const [input, ...arr] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-
 let [N, M] = input.split(' ').map(v => +v);
 let graph = arr.map(v => v.split(' ').map(d => +d));
-// console.log(graph)
-let visited = Array.from(Array(M), () => new Array(N).fill(false))
-// let dist = Array.from(Array(M), () => new Array(N).fill(0))
-
+let visited = Array.from(Array(M), () => new Array(N).fill(0))
 let queue = [];
-// console.log(graph);
-
+let answer = [];
+let max = 0;
+let BFS = (y, x) => {
+  let dx = [1, 0, -1, 0];
+  let dy = [0, -1, 0, 1];
+  let index = 0;
+  while(index !== queue.length) {
+    let [tempY, tempX] = queue[index];
+    for(let i = 0; i < dx.length; i++) {
+      let nx = tempX + dx[i];
+      let ny = tempY + dy[i];
+      // if(nx < N && nx >= 0 && ny < M && ny >= 0) {
+      if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue; 
+      // console.log(visited[ny][nx], nx, ny)
+      if(visited[ny][nx] >= 0) continue;
+      visited[ny][nx] = visited[tempY][tempX] + 1;
+      max = Math.max(max, visited[ny][nx]);
+      queue.push([ny, nx]);
+    }
+    index++;
+  }
+} 
 for(let i = 0; i < M; i++) {
   for(let j = 0; j < N; j++) {
     if(graph[i][j] === 1) {
-      visited[i][j] = true;
       queue.push([i, j]);
     }
 
-    // if(graph[i][j] === 0) {
-    //   dist[i][j] = -1;
-    // }
-  }
-}
-
-
-// console.log(dist);
-
-let max = 0;
-let index = 0;
-let dx = [1, 0, -1, 0];
-let dy = [0, -1, 0, 1];
-
-while(queue.length) {
-  let [tempY, tempX] = queue.shift();
-  for(let i = 0; i < dx.length; i++) {
-    let nx = tempX + dx[i];
-    let ny = tempY + dy[i];
-    if(nx < N && nx >= 0 && ny < M && ny >= 0) {
-      // if(graph[ny][nx] === 0 && !visited[ny][nx]) {
-      if(graph[ny][nx] >= 0 && !visited[ny][nx]) {
-        visited[ny][nx] = true;
-        graph[ny][nx] = graph[tempY][tempX] + 1;
-        // if(max < graph[ny][nx]) {
-        //   max = graph[ny][nx];
-        // }
-
-        max = Math.max(max, graph[ny][nx]);
-        queue.push([ny, nx]);
-      }
-      // if(visited[ny][nx] >= 0) {
-      //   continue;
-      // }
-      // visited[ny][nx] = visited[tempY][tempX] + 1;
-      // queue.push([ny, nx]);
+    if(graph[i][j] === 0) {
+      visited[i][j] = -1;
     }
   }
 }
-
-// console.log(max);
-
-let answer = [];
-
+BFS();
 for(let i = 0; i < M; i++) {
   for(let j = 0; j < N; j++) {
-    if(graph[i][j] === 0) {
+    if(visited[i][j] === -1) {
       answer.push(-1);
       break;
     } 
   }
 }
-
-if(max === 0) {
-  answer.push(0);
-} else {
-  answer.push(max - 1);
-}
-
-// console.log(visited);
+answer.push(max);
 console.log(answer[0]);
-
-// console.log(visited);
-// console.log(graph)
-// console.log(count);
