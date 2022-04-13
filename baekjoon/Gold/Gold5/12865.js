e235904fea96e1604dea686fe47851a3f5a8aa45
Captainjack-kor <jennystr233@gmail.com>
2022-04-13 13:38:33 +0900

    hmmmmm

--- baekjoon/Gold/Gold5/12865.js
+++ baekjoon/Gold/Gold5/12865.js
@@ -1,8 +1,8 @@
 const [input, ...arr] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let bag = input[0].split(' ')[1];
+let bag = input.split(' ').map(v => +v);
 
-let list = arr.map(v => v.split(' ').map(v => +v));
+let list = arr.map(v => v.split(' ').map(v => +v)).sort((a, b) => b[0] - a[0]);
 
 console.log(bag);
-console.log(...list);
+console.log(list);
---
+++ baekjoon/Silver/Silver3/15649.js
@@ -0,0 +1,3 @@
+const [N, M] = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(v => +v);
+
+// for(let )
