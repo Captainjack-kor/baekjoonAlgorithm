f575cc9aaa3e9859e67fe7b3230cd2c255cb478c
Captainjack-kor <jennystr233@gmail.com>
2022-04-13 16:02:01 +0900

    sync

--- baekjoon/Gold/Gold5/1068.js
+++ baekjoon/Gold/Gold5/1068.js
@@ -4,6 +4,9 @@ let inputValue = +input[0];
 let tree = input[1].split(' ').map(v => +v).sort((a, b) => a - b);
 let target = +input[2];
 let count = 0;
+if(inputValue === 1) {
+  return console.log(1);
+}
 for(let i = 1; i <= inputValue; i++) {
   if(tree[i] >= target) {
     break;
