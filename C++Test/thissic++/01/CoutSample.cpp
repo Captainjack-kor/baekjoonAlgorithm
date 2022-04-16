8b9759f816b6f5cbb6ec379176221dd14891119f
Jack Lim <jennystr233@gmail.com>
2022-04-16 18:18:09 +0900

    merged

---
+++ C++Test/thissic++/01/CoutSample.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// using namespace std;
+
+
+int main(int argc, char* argv[]) {
+  std::cout << 10 << std::endl;
+  std::cout << 10U << std::endl;
+  std::cout << 10.5F << std::endl;
+  std::cout << 10.5 << std::endl;
+  std::cout << 3 + 4 << std::endl;
+
+  return 0;
+}
