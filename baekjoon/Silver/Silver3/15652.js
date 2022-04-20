2d1f4e1b6250b1626977b68288720e84d0563b09
Captainjack-kor <jennystr233@gmail.com>
2022-04-20 09:52:05 +0900

    백트래킹..

---
+++ baekjoon/Silver/Silver3/15652.js
@@ -0,0 +1,31 @@
+const [N, M] = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(v => +v);
+let arr = [];
+let isUsed = [];
+let answer = [];
+let temp = "";
+function dps(k) {
+  if(k === M) {
+    for(let i = 0; i < M; i++) {
+      temp += arr[i] + ' ';
+    }
+    answer.push(temp.trim());
+    temp = "";
+    
+    return;
+  }
+  let st = 1;
+  if(k !== 0) {
+    st = arr[k - 1];
+    // st = arr[k - 1] + 1;
+  }
+  for(let i = st; i <= N; i++) {
+    if(!isUsed[i]) {
+      arr[k] = i;
+      // isUsed[i] = true;
+      dps(k + 1);
+      // isUsed[i] = false;
+    }
+  }
+}
+dps(0);
+console.log(answer.join('\n'));
